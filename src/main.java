c126dfbbf0c8b809e53572efbe4d6f87e1fc2769
siwu-945 <sl3659@drexel.edu>
2024-02-12 03:08:18 -0500

    cleaned up code

--- src/main.java
+++ src/main.java
@@ -6,6 +6,66 @@ import java.util.ArrayList;
 import java.util.Map;
 
 public class main {
+    private static String generateFieldParameter(int[] field) {
+        String para = "";
+        for (int i = 0; i < field.length; i++) {
+            if (i != field.length - 1) {
+                para += field[i] + " , ";
+            } else {
+                para += field[i];
+            }
+        }
+        return para;
+    }
+
+    private static ArrayList<String> getTbleNames(Map<String, BasicBlock> blocks) {
+        ArrayList<String> vtbleNames = new ArrayList<>();
+
+        blocks.forEach((key, value) -> {
+            if (!key.equals("main")) {
+                vtbleNames.add(key);
+            }
+        });
+
+        return vtbleNames;
+    }
+
+    private static ArrayList<String> generateGlobalFieldArray(ArrayList<String> vtbleNames, Map<String, ArrayList<String>> totalFields) {
+        ArrayList<String> globalArray = new ArrayList<>();
+        for (String tbleName : vtbleNames) {
+            ArrayList<String> fields = totalFields.get(tbleName);
+            for (int i = 2; i < fields.size(); i++) {
+                if (!globalArray.contains(fields.get(i))) {
+                    globalArray.add(fields.get(i));
+                }
+            }
+        }
+        return globalArray;
+    }
+
+    private static ArrayList<String> generateGlobalArrayString(ArrayList<String> globalArray, Map<String, ArrayList<String>> totalFields, ArrayList<String> vtbleNames) {
+        ArrayList globalArrayList = new ArrayList();
+        int fieldSize = globalArray.size();
+        int[] fieldsX = new int[fieldSize];
+        for (String tbleName : vtbleNames) {
+            ArrayList<String> fields = totalFields.get(tbleName);
+            fields.remove(0);
+            fields.remove(0);
+            for (int i = 0; i < fieldSize; i++) {
+                String currentField = globalArray.get(i);
+                int fieldID = fields.indexOf(currentField) + 2;
+                if (fieldID > 1) {
+                    fieldsX[i] = fieldID;
+                } else {
+                    fieldsX[i] = 0;
+                }
+            }
+            globalArrayList.add(String.format("global array vfields%s: { %s }", tbleName, generateFieldParameter(fieldsX)));
+            fieldsX = new int[fieldSize];
+        }
+        return globalArrayList;
+    }
+
     public static void main(String[] args) {
 
         String irTest1 =
@@ -42,67 +102,17 @@ public class main {
         ArrayList<BasicBlock> myBlocks = new ArrayList<BasicBlock>();
         ArrayList<IRStatement> myIRStatements = new ArrayList<>();
 
-//        ClassNode myClass = myParser.parseClass(irClassTest);
-//        System.out.println(myClass);
-//
-//        ArrayList<ASTStatement> codeToStas = myParser.parseStatementBlock(irTest1);
-//        GlobalDataSegment classArrays = myParser.checkForClass(codeToStas);
-//        myBlocks.add(new BasicBlock(myIRStatements, "initial"));
-//        myIR.addToBB(codeToStas, myBlocks, "initial");
-//        for (BasicBlock block : myBlocks) {
-//            for (IRStatement irLine : block.getIRStatements()) {
-//                System.out.println(irLine);
-//            }
-//        }
-
         Map<String, BasicBlock> blocks = myParser.readingSource(wholeSource);
         Map<String, ArrayList<String>> totalFields = myParser.generateFields(wholeSource);
-        ArrayList<String> vtbleNames = new ArrayList<>();
-        ArrayList<String> globalArray = new ArrayList<>();
 
-        blocks.forEach((key, value) -> {
-            if (!key.equals("main")) {
-                vtbleNames.add(key);
-            }
-        });
+        ArrayList<String> vtbleNames = getTbleNames(blocks);
+        ArrayList<String> globalArray = generateGlobalFieldArray(vtbleNames, totalFields);
+        ArrayList<String> globalArrayLists = generateGlobalArrayString(globalArray, totalFields, vtbleNames);
 
-        for (String tbleName : vtbleNames) {
-            ArrayList<String> fields = totalFields.get(tbleName);
-            for (int i = 2; i < fields.size(); i++) {
-                if (!globalArray.contains(fields.get(i))) {
-                    globalArray.add(fields.get(i));
-                }
-            }
-        }
-        int fieldSize = globalArray.size();
-        int[] fieldsX = new int[fieldSize];
-        for (String tbleName : vtbleNames) {
-            ArrayList<String> fields = totalFields.get(tbleName);
-            fields.remove(0);
-            fields.remove(0);
-            for (int i = 0; i < fieldSize; i++) {
-                String currentField = globalArray.get(i);
-                int fieldID = fields.indexOf(currentField) + 2;
-                if (fieldID > 1) {
-                    fieldsX[i] = fieldID;
-                } else {
-                    fieldsX[i] = 0;
-                }
-            }
-            System.out.println(generateFieldParameter(fieldsX));
-            fieldsX = new int[fieldSize];
+        for (String array : globalArrayLists) {
+            System.out.println(array);
         }
     }
 
-    private static String generateFieldParameter(int[] field) {
-        String para = "";
-        for (int i = 0; i < field.length; i++) {
-            if (i != field.length - 1) {
-                para += field[i] + " , ";
-            } else {
-                para += field[i];
-            }
-        }
-        return para;
-    }
+
 }
